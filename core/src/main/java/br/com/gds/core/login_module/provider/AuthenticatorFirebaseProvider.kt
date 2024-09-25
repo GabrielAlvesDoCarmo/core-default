f77b1bc4e6193e0cad87c0b3aaac33424d9832a0
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2024-09-25 13:08:41 -0300

    network included state

--- core/src/main/java/br/com/gds/core/login_module/provider/AuthenticatorFirebaseProvider.kt
+++ core/src/main/java/br/com/gds/core/login_module/provider/AuthenticatorFirebaseProvider.kt
@@ -1,5 +1,6 @@
 package br.com.gds.core.login_module.provider
 
+import br.com.gds.core.login_module.commons.network.state.RequestState
 import br.com.gds.core.login_module.model.login.LoginRequest
 import br.com.gds.core.login_module.model.login.LoginResponse
 import br.com.gds.core.login_module.model.register.RegisterRequest
@@ -8,19 +9,19 @@ import br.com.gds.core.login_module.model.resetpassword.ResetPasswordRequest
 
 sealed interface AuthenticatorFirebaseProvider {
     interface Login {
-        suspend fun login(login: LoginRequest): LoginResponse
+        suspend fun login(login: LoginRequest): RequestState<LoginResponse>
     }
 
     interface Register {
-        suspend fun register(registerRequest: RegisterRequest): RegisterResponse
+        suspend fun register(registerRequest: RegisterRequest): RequestState<RegisterResponse>
     }
 
     interface ResetPassword <T> {
-        suspend fun resetPassword(email: ResetPasswordRequest): T
+        suspend fun resetPassword(email: ResetPasswordRequest): RequestState<T>
     }
 
     interface AuthFirebase <T> {
-        suspend fun isUserLogged(): T
-        suspend fun logout(): T
+        suspend fun isUserLogged():  RequestState<T>
+        suspend fun logout():  RequestState<T>
     }
 }
