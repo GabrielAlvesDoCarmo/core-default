3c8d32d561da83c7eb25598c3e4c6e03ea234cd8
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2024-09-25 23:49:27 -0300

    change auth response

--- core/src/main/java/br/com/gds/core/login_module/model/login/LoginResponse.kt
+++ core/src/main/java/br/com/gds/core/login_module/model/login/LoginResponse.kt
@@ -1,9 +1,7 @@
 package br.com.gds.core.login_module.model.login
 
-import br.com.gds.core.login_module.commons.model.UserAuth
-
 data class LoginResponse(
     val success: Boolean,
     val message: String? = null,
-    val userInfo: UserAuth? = null,
+    val additionalInfo: Any? = null,
 )
--- core/src/main/java/br/com/gds/core/login_module/model/register/RegisterResponse.kt
+++ core/src/main/java/br/com/gds/core/login_module/model/register/RegisterResponse.kt
@@ -1,9 +1,7 @@
 package br.com.gds.core.login_module.model.register
 
-import br.com.gds.core.login_module.commons.model.UserAuth
-
 data class RegisterResponse(
     val success: Boolean,
     val message: String? = null,
-    val userInfo: UserAuth? = null,
+    val additionalInfo: Any? = null,
 )
